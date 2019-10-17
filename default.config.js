8cb87afea1504b2230808158cb0987dabe852366
Joe Maller <joe@joemaller.com>
2019-10-17 17:27:27 -0400

    update entry in default config

--- default.config.js
+++ default.config.js
@@ -1,19 +1,17 @@
 require("dotenv").config();
 
 const pkg = require("./package.json");
-const themeName = process.env.NAME || pkg.name || 'theme-name';
+const themeName = process.env.NAME || pkg.name || "theme-name";
 
 module.exports = {
   src: `./wp-content/themes/${themeName}/src`,
   dist: `./wp-content/themes/${themeName}/dist`,
-  entry: {
-    index: ["./js/index.js", "./sass/main.scss"],
-    admin: "./js/admin.js",
-    editor: "./js/editor-blocks.js"
-  },
-  // TODO: Switch this back to an array once gulpfile is gone, either that or rename the
-  //       stylesheet entry file to prevent writing an empty js file
-  entry: ["./js/index.js", "./js/admin.js", "./js/editor-blocks.js"],
+  entry: [
+    "./js/main.js",
+    "./js/admin.js",
+    "./js/editor.js",
+    "./sass/main.scss"
+  ],
   publicPath: `/wp-content/themes/${themeName}/dist/`,
 
   // TODO: This should be automatic, maybe valid entries are: true, false, null, string (a valid url)?
