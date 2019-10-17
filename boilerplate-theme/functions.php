be7f337ce3cb25eb61f3bcbb2fb55b47baf84264
Joe Maller <joe@joemaller.com>
2019-10-17 17:23:58 -0400

    theme boilerplate

---
+++ boilerplate-theme/functions.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ideasonpurpose;
+
+$autoloader = require __DIR__ . '/vendor/autoload.php';
+
+new ThemeInit();
+
+if (!defined('VERSION')) {
+    define('VERSION', defined('WP_DEBUG') ? time() : wp_get_theme()->get('Version'));
+}
+
+/**
+ * Initialize ideasonpurpose/GoogleAnalytics
+ */
+new GoogleAnalytics('UA-2565788-3');
+
+/**
+ * Initialize a logger
+ */
+$Log = new ThemeInit\Logger('iop');
+
+/**
+ * Initialize our SVG Library
+ */
+$SVG = new SVG();
+
+/**
+ * Register Custom Taxonomies
+ */
+// new Taxonomy\NAME();
+
+/**
+ * Register Custom Post Types
+ */
+// new CPT\NAME(22);
+
+/**
+ * Set separators for the WordPress admin
+ */
+new ThemeInit\Admin\Separators(20, 26, 28);
+
+/**
+ * Register Custom Widegts
+ */
+// new Widgets\NAME();
+
+/**
+ * Load Scripts
+ */
+// TODO: What happens if this file doesn't exist yet?
+new ThemeInit\Manifest(get_template_directory() . '/dist/dependency-manifest.json');
---
+++ boilerplate-theme/src/js/main.js
@@ -0,0 +1,6 @@
+/**
+ * If we import main.scss here, remove it from the entry array in ideasonpurpose.config.js
+ */
+// require('../sass.main.scss')
+
+console.log('JS Loaded');
