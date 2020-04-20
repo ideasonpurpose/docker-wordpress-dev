535596546d02b59520cc7433c3d16f8717bd4a8d
Joe Maller <joe@joemaller.com>
2020-04-20 03:03:35 -0400

    add sass option to default config

--- default.config.js
+++ default.config.js
@@ -14,6 +14,9 @@ module.exports = {
   ],
   publicPath: `/wp-content/themes/${themeName}/dist/`,
 
+  // OPTIONAL: Specify a Sass implementation, accepts `node-sass` (default) and `sass` (Dart Sass)
+  sass: 'node-sass',
+
   // TODO: This should be automatic, maybe valid entries are: true, false, null, string (a valid url)?
   proxy:
     "http://devserver-proxy-token--d939bef2a41c4aa154ddb8db903ce19fff338b61"
