a3c75eb9ec2552ed546f14baec5866cde2d033a4
Joe Maller <joe@joemaller.com>
2022-03-13 19:18:10 -0400

    0.9.1

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 # https://hub.docker.com/_/wordpress/
 FROM wordpress:5.9.2-php8.0-apache
 
-LABEL version="0.9.0"
+LABEL version="0.9.1"
 
 # Add `wp` user and group, then add `www-data` user to `wp` group
 RUN addgroup  --gid 1000 wp \
--- bin/permissions.sh
+++ bin/permissions.sh
@@ -3,7 +3,7 @@
 # This script is a part of the ideasonpurpose/docker-wordpress-dev project
 # https://github.com/ideasonpurpose/docker-wordpress-dev
 #
-# Version: 0.9.0
+# Version: 0.9.1
 
 # This script corrects permissions of known files in our WordPress boilerplate.
 # The script is run from Docker as root, with a $OWNER_GROUP envvar set to "$UID:$GID"
