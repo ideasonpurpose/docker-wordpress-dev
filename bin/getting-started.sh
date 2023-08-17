f4b167e67b55a10125e23d9eea479c9369e14e3c
Joe Maller <joe@joemaller.com>
2023-08-16 23:07:17 -0400

    1.1.0

--- bin/getting-started.sh
+++ bin/getting-started.sh
@@ -3,7 +3,7 @@
 # This script is a part of the ideasonpurpose/docker-wordpress-dev project
 # https://github.com/ideasonpurpose/docker-wordpress-dev
 #
-# Version: 1.0.4
+# Version: 1.1.0
 
 # Getting Started message
 
