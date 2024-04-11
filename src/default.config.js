72756e209ccb24c6695cb0e8e3a11cb308bccce0
Joe Maller <joe@joemaller.com>
2024-04-11 09:09:07 -0400

    update default config

--- src/default.config.js
+++ src/default.config.js
@@ -1,25 +1,16 @@
-const pkg = require("./package.json");
-const themeName = pkg.name || "ioptheme";
+import { readPackageUp } from "read-package-up";
+const themeName = (await readPackageUp()).packageJson.name;
 
-module.exports = {
+export default {
   src: `./wp-content/themes/${themeName}/src`,
   dist: `./wp-content/themes/${themeName}/dist`,
-  entry: [
-    "./js/main.js",
-    "./js/admin.js",
-    "./js/editor.js",
-    "./sass/main.scss",
-  ],
+
+  entry: ["./js/main.js", "./js/admin.js", "./js/editor.js"],
+
   publicPath: `/wp-content/themes/${themeName}/dist/`,
 
-  // enable polling on Windows (no iNotify events), does `isWindows` work?
-  // This property can be a boolean or an integer > 250
-  // NOTE: iNotify events sometimes stop working on macOS, restart Docker to get them back
-  // usePolling: false,
+  // sass: "sass-embedded",
+  // esTarget: "es2020",
 
-  // OPTIONAL: Specify a Sass implementation, defaults to sass-embedded. Override with `sass` or `node-sass`.
-  // sass: "sass",
-
-  // Set the webpack devtool option for sourcemaps
   devtool: "source-map",
 };
