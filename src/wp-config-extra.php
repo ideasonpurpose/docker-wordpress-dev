5610a1d561428ca489e1951544cb14b7b5b57a0c
Joe Maller <joe@joemaller.com>
2025-03-17 12:36:50 -0400

    add WP_DEVELOPMENT_MODE = core constant, closes #71

--- src/wp-config-extra.php
+++ src/wp-config-extra.php
@@ -1,51 +1,13 @@
 <?php
 
-/**
- * TODO: Move this into a separate file
- * TODO: No longer necessary?
- * TODO: Clean up this mess
- *
- * Get this container's ephemeral port on the Host via the Docker API
- */
-
-// translating this
-// @link https://sleeplessbeastie.eu/2021/12/13/how-to-query-docker-socket-using-curl/
-//    curl --silent -XGET --unix-socket /run/docker.sock http://localhost/containers/0a8a082812a1/json | jq .
-
-$protocol = isset($_SERVER["HTTPS"]) ? 'https' : 'http';
-// $hostname = gethostname();
-// $socket = "/run/docker.sock";
-// $req  = "http://localhost/containers/{$hostname}/json";
-// $host = "{$protocol}://{$_SERVER['HTTP_HOST']}";
-
-// $curl = curl_init();
-// curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($curl, CURLOPT_UNIX_SOCKET_PATH, $socket);
-// curl_setopt($curl, CURLOPT_URL, $req);
-// $info = curl_exec($curl);
-// $info = json_decode($info);
-
-// $wp_port =  (integer) $info->NetworkSettings->Ports->{'80/tcp'}[0]->HostPort;
-
-
-// d($_SERVER, $hostname, $socket, $req, $info , $wp_port);
-
-
+$protocol = isset($_SERVER['HTTPS']) ? 'https' : 'http';
 
 /**
  * Extra ideasonpurpose dev settings
  *
  * Since the 5.7 WordPress image, this file is included instead of parsed
  * ref: https://github.com/docker-library/wordpress/commit/891b7108294a629e6cc16c2f4bf643d9475894cc
- *
- * Serve WordPress internally from Docker's internal IP address
  */
-// define('WP_HOME', 'http://' . $_SERVER['SERVER_ADDR']);
-// define('WP_SITEURL', 'http://' . $_SERVER['SERVER_ADDR']);
-// define('WP_HOME', 'http://localhost:' . $_ENV['WP_PORT']);
-// define('WP_SITEURL', 'http://localhost:' . $_ENV['WP_PORT']);
-// define('WP_HOME', "http://localhost:{$wp_port}");
-// define('WP_SITEURL', "http://localhost:{$wp_port}");
 define('WP_HOME', "{$protocol}://{$_SERVER['HTTP_HOST']}");
 define('WP_SITEURL', "{$protocol}://{$_SERVER['HTTP_HOST']}");
 
@@ -60,6 +22,7 @@ define('WP_SITEURL', "{$protocol}://{$_SERVER['HTTP_HOST']}");
 if (WP_DEBUG) {
     define('WP_DEBUG_LOG', '/var/log/wordpress/debug.log');
     define('WP_DEBUG_DISPLAY', true);
+    define('WP_DEVELOPMENT_MODE', 'core');
     define('SCRIPT_DEBUG', true);
     define('SAVEQUERIES', true);
 }
