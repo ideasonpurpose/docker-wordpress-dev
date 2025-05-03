3b89c5ae3e6bcb3060019bb5b40a68c03a4e90bf
Joe Maller <joe@joemaller.com>
2025-05-03 19:43:21 -0400

    1.7.5

--- bin/pull.sh
+++ bin/pull.sh
@@ -3,7 +3,7 @@
 # This script is a part of the ideasonpurpose/docker-wordpress-dev project
 # https://github.com/ideasonpurpose/docker-wordpress-dev
 #
-# Version: 1.7.4
+# Version: 1.7.5
 
 # style helpers
 RESET="\033[0m"
